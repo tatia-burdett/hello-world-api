48f177f89e9b2a490d9607c6a2e1a5782ecd1e90
Tatia burdett <tatiaburdett@gmail.com>
2021-03-27 13:05:32 -0700

    start setting up tests

--- test/hello_world-endpoints.spec.js
+++ test/hello_world-endpoints.spec.js
@@ -2,4 +2,16 @@ const { expect } = require('chai')
 const knex = require('knex')
 const supertest = require('supertest')
 const app = require('../src/app')
+const { makeCommentArray } = require('./comment-fixtures')
 
+describe('Hello World Endpoints', () => {
+  let db
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: 'process.env.TEST_DATABASE_URL'
+    })
+    app.set('db', db)
+  })
+})
