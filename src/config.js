0bbee46f799225c599d71f384cb3c285a6736c91
Tatia burdett <tatiaburdett@gmail.com>
2021-03-28 13:30:04 -0700

    DB -> DATABASE

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://tatjanacarney@localhost/hello-world',
-  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://tatjanacarney@localhost/hello-world-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://tatjanacarney@localhost/hello-world',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://tatjanacarney@localhost/hello-world-test'
 }
